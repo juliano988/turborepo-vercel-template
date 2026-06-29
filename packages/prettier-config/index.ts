31bfdec2fa589f7e04922a9ad09cee5af4e6c8fd
Júlio Faria <julio-pinheiroo@hotmail.com>
2026-06-29 08:50:58 -0300

    feat: atualizar configuração do Prettier com novas opções de formatação

--- packages/prettier-config/index.ts
+++ packages/prettier-config/index.ts
@@ -3,9 +3,15 @@ import type { Config } from "prettier";
 const config: Config = {
   semi: true,
   singleQuote: false,
+  jsxSingleQuote: false,
   tabWidth: 2,
+  useTabs: false,
   trailingComma: "es5",
   printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
 };
 
 export default config;
