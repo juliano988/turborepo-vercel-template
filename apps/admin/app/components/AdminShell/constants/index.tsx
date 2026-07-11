4f880c19f8a80c395505056c46a750cad3fedeeb
Júlio Faria <julio-pinheiroo@hotmail.com>
2026-07-10 22:02:10 -0300

    Refactor code structure for improved readability and maintainability

--- apps/admin/app/components/AdminShell/constants/index.tsx
+++ apps/admin/app/components/AdminShell/constants/index.tsx
@@ -1,24 +1,29 @@
-import { FileText, LayoutDashboard, Settings, Users } from "@repo/ui";
+import {
+  DashboardOutlined,
+  FileTextOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 export const menuRoutes = [
   {
     path: "/",
     name: "Dashboard",
-    icon: <LayoutDashboard size={16} />,
+    icon: <DashboardOutlined />,
   },
   {
     path: "/usuarios",
     name: "Usuarios",
-    icon: <Users size={16} />,
+    icon: <UserOutlined />,
   },
   {
     path: "/conteudo",
     name: "Conteudo",
-    icon: <FileText size={16} />,
+    icon: <FileTextOutlined />,
   },
   {
     path: "/configuracoes",
     name: "Configuracoes",
-    icon: <Settings size={16} />,
+    icon: <SettingOutlined />,
   },
 ];
